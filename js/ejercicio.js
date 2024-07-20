a20bd68e62145122bb09811d3e8d371e58a2813d
Sena <Sena@IBADFRCIIOP010.FormacionSena.RED>
2024-07-19 19:08:18 -0600

    javaScr

---
+++ js/ejercicio.js
@@ -0,0 +1,12 @@
+document.write("hola mundo 💖");
+
+var nombre="Marlon"
+var apellido="Leon"
+const nacimiento="2001"
+let sueldo=120000
+const fecha=new Date();
+const fechaActual=fecha.getFullYear();
+let edad=fechaActual-nacimiento
+
+document.write("Hola " + nombre + ". Su apellido es " + apellido + ". Nació en el año  " + nacimiento + ".  y tiene  " + edad + ". años de edad.  Su sueldo es de  " + sueldo);
+document.write(". tiene "+ edad +". años de edad")
