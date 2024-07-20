a7d844909665f5e0e3bcc6ea8100540bbc6e3f65
Sena <Sena@IBADFRCIIOP010.FormacionSena.RED>
2024-07-19 19:34:16 -0600

    firts

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,8 +1,12 @@
 document.write("hola mundo 💖");
 
-var nombre="Marlon"
-var apellido="Leon"
-const nacimiento="2001"
+var nombre=prompt("ingrese su nombre : ")
+var apellido=prompt("ingrese tu apellido: ")
+var nacimiento=parseInt(prompt("ingrese el año de naciemiemto: "))
+
+/**  var nombre="Marlon"
+var apellido="Leon"  
+const nacimmiento="2001"*/
 let sueldo=120000
 const fecha=new Date();
 const fechaActual=fecha.getFullYear();
@@ -10,3 +14,10 @@ let edad=fechaActual-nacimiento
 
 document.write("Hola " + nombre + ". Su apellido es " + apellido + ". Nació en el año  " + nacimiento + ".  y tiene  " + edad + ". años de edad.  Su sueldo es de  " + sueldo);
 document.write(". tiene "+ edad +". años de edad")
+
+
+
+
+
+console.log(typeof(nombre))
+console.log(typeof(nacimiento))
