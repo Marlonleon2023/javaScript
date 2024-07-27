7c673541126f6ecd46c21c5139fb33e8bf4e43eb
Sena <Sena@IBAAPRCIIFOP901.FormacionSena.RED>
2024-07-26 20:35:14 -0500

    gg-button

--- js/buton.js
+++ js/buton.js
@@ -1,16 +1,16 @@
 function mostrar(){
+   
+    seguir="si"
     contador=0
-seguir="si"
 
+    while(seguir=="si"){
+        
+        let cupos=parseInt(prompt("ingresa el numero de cupos a comprar: "))
+        contador+=cupos
 
-while(seguir=="si"){
-
-    let cupos=parseInt(prompt("ingresa el numero de cupos a comprar: "))
-    contador+=cupos
-
-    if(contador==50){
-        seguir="no"
+        if(contador==50){
+            seguir="no"
+        }
+        document.write("cupos comprados "+cupos+"<br>"+"total: "+contador+"<br>")
     }
-    document.write("cupos comprados "+cupos+"<br>"+"total: "+contador+"<br>")
-}
 }
