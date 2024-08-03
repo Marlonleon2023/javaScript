1cb7556e6b3e1f84233f62f381d8987c5b4a8440
Sena <Sena@IBAAPRCIIFOP901.FormacionSena.RED>
2024-08-02 20:36:03 -0500

    firtss

--- js/activity.js
+++ js/activity.js
@@ -1,19 +1,52 @@
-//capture el nombre de un estudiaste e ingrese 3 notas y calcular el promedio
-//si el pomerdio de 0 a 3 no aprobo
-//promedio 3.1 a 3,5 recuperacion
-//promedio 3.6 a 5 aprobo
+function mostrar() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let opcion = parseInt(document.getElementById("operacion").value);
+    
+    let res = document.getElementById("result");
 
-var estudiante=prompt("ingresa tu nombre estudiante: ")
-var nota1=parseFloat(prompt("ingresa tu primer nota"))
-var nota2=parseFloat(prompt("ingresa tu segunda nota"))
-var nota3=parseFloat(prompt("ingresa tu tercera nota"))
+    switch (opcion) {
+        case 1:
+            resultado = division(n1, n2);
+            break;
+        case 2:
+            resultado = suma(n1, n2);
+            break;
+        case 3:
+            resultado = resta(n1, n2);
+            break;
+        case 4:
+            resultado = multiplicar(n1, n2);
+            break;
+        case 5:
+            resultado = potencia(n1, n2);
+            break;
+        default:
+            res.innerHTML = 'Seleccione una opción valida';
+            return;
+    }
+    res.innerHTML = `Total: ${resultado}`;
+}
 
-let promedio=(nota1+nota2+nota3)/3
+function suma(n1, n2) {
+    return n1 + n2;
+}
 
-if (promedio<=3) {
-    document.write(" no aprobo"+" el estudiante "+ estudiante + " con un promedio de : " + promedio )
-}else if  (promedio<=3.5){
-    document.write(" recuperacion "+" el estudiante "+ estudiante + " con un promedio de : " + promedio )
-}else if(promedio>=3.6){
-    document.write("aprobo "+" el estudiante "+ estudiante + " con un promedio de : " + promedio)
+function division(n1, n2) {
+    if (n2 === 0) {
+        return 'No se puede dividir por cero';
+    }
+    return n1 / n2;
+}
+
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+
+function resta(n1, n2) {
+    return n1 - n2;
+}
+
+function potencia(n1, n2) {
+    return Math.pow(n1, n2);
 }
